440a16a65bcc3f12932c637e93ed72b4a57727c9
richardmillen <develop@richardmillen.com>
2017-11-09 13:23:49 +0000

    asyncsrv example now implemented.

--- async-client-server-pattern/asyncsrv.cpp
+++ async-client-server-pattern/asyncsrv.cpp
@@ -5,11 +5,14 @@
 #include "zuse.hpp"
 
 #include <iostream>
+#include <string>
 #include <thread>
 #include <mutex>
 #include <random>
 
 using namespace std;
+
+#define MATCH_ID	"[0-9A-F]{4}-[0-9A-F]{4}"
 
 mutex mut;
 
@@ -41,27 +44,28 @@ void client_node() {
 	
 	unsigned req_count;
 	auto recv_count = 0;
+	auto id = random_id();
 	
-	recving.on_enter([&](zuse::context_t& c) {
+	recving.on_enter([&](zuse::event::context_t& c) {
 		recv_count = 0;
 	});
-	recving.on_recv(inbound, [&](zuse::context_t& c) {
-		// TODO: print received message
+	recving.on_recv(inbound, [&](zuse::event::context_t& c) {
+		cout "client [" << id << "]: " << c.frame() << endl;
 		++recv_count;
 	}).next_state(sending);
-	recving.on_exit([](zuse::context_t& c) {
-		c.next_stream() << "request " << ++req_count;
+	recving.on_exit([&](zuse::event::context_t& c) {
+		c.defer([&]() { c.send() << "request " << ++req_count; });
 	});
 	
 	sending.add_condition([&](zuse::state_t& s) {
 		return recv_count >= 100;
 	});
 	
-	sending.on_send(request, [](zuse::context_t& c) {
-		cout << "client: sending '" << c.frame() << "'..." << endl;
+	sending.on_send(request, [](zuse::event::context_t& c) {
+		cout << "client [" << id << "]: sending '" << c.frame() << "'..." << endl;
 	}).next_state(recving);
 	
-	context.connect("tcp://localohst:5570", zuse::connect_type::dealer, random_id());
+	context.connect("tcp://localohst:5570", zuse::connect_type::dealer, id);
 	
 	context.poll(recving, 10ms);
 }
@@ -82,9 +86,16 @@ void server_node() {
 		w.join();
 }
 
+// server worker thread.
+//
+// a zmq socket is paired with the thread in which it's created (context->connect). 
+// this means that unless you call connect more than once in a thread you don't need
+// to work with the socket as zuse will use handle that for you.
+//
+// the worker can be in two states; receiving or sending. this was really done for 
+// demonstration purposes as the code is so simple it only needs a single state to
+// receive requests and send replies.
 void server_work(zuse::context_t* context, int num) {
-	auto id(to_string(num));
-	
 	random_device rd;
 	mt19937 eng(rd());
 	uniform_int_distribution<> rep_distr(1, 5);
@@ -93,30 +104,41 @@ void server_work(zuse::context_t* context, int num) {
 	state_t recving("receiving");
 	state_t sending("sending");
 	
-	auto worker = context->connect("inproc://backend", zuse::connect_type::dealer);
+	context->connect("inproc://backend", zuse::connect_type::dealer);
 	
-	zuse::message_t message("request/reply message", {zuse::message_t::any, zuse::message_t::any});
+	zuse::message_t request("request message", {{"id", MATCH_ID}, {"msg", zuse::message_t::any}});
+	zuse::message_t reply("loop and send replies", {{"echo-count", R"(\d+)"}, {"id", MATCH_ID}, {"msg", zuse::message_t::any}});		
 	
-	// TODO: define this is a type that indicates that it's private i.e. won't be sent over the socket
-	zuse::message_t loop("loop and send replies", {R"(\d+)", zuse::message_t::any, zuse::message_t::any});		
-	
-	recving.on_recv(worker, message, [](zuse::context_t& c) {
-		cout << "server [" << id << "]: echoing (" << c.frame(0) << "," << c.frame(1)  << ")..." << endl;
-		c.next_send(rep_distr(env));
-		c.next_send(c.frames());
+	recving.on_recv(request, [](zuse::event::context_t& c) {
+		cout << "server [" << num << "]: echoing (" << c.frame(0) << "," << c.frame(1)  << ")..." << endl;
+		
+		c.defer([&]() {	c.raise_event({ rep_distr(env), c.frame(0), c.frame(1) }); });
 	}).next_state(sending);
 	
-	sending.on_send(loop, [&](zuse::context_t& c) {
-		auto echo_count = stoi(c.private_frame());
+	sending.on_event(reply, [&](zuse::event::context_t& c) {
+		auto echo_count = stoi(c.frame(0));
 		for (auto i = 0; i < echo_count; ++i) {
 			this_thread::sleep_for(sleep_distr(eng));
-			c.send(c.frames());
+			c.send(c.frames().begin() + 1, c.frames().end());
 		}
-	});
-	
-	sending.on_send(worker, message, [&](zuse::context_t& c) {
-	});
+	}).next_state(recving);
 }
+
+string random_id() {
+	random_device rd;
+	mt19937 eng(rd());
+	uniform_int_distribution<> distr(0x00000, 0x10000);
+	
+	char identity[10] = {};
+	sprintf(identity, "%04X-%04X", distr(eng), distr(eng));
+	return string(identity);
+}
+
+
+
+
+
+
 
 
 
